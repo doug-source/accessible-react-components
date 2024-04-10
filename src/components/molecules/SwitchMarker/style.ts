13e5903e3a09ce7c0b666eef5fe0ca966a104a7a
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-10 02:37:25 -0300

    :broom: cleanup: Removing commented code

--- src/components/molecules/SwitchMarker/style.ts
+++ src/components/molecules/SwitchMarker/style.ts
@@ -13,10 +13,8 @@ export const SwitchMarker_ = styled.div<SwitchMarkerProps_>`
     border-width: ${({ $borderWidth }) => $borderWidth};
     border-style: solid;
     border-color: #000;
-    /* border: 0.125rem solid black; */
     width: 2.5rem;
     border-radius: 0.75rem;
-    /* padding: 0.125rem; */
     padding: ${({ $padding }) => $padding};
     ${({ $height, $padding, $borderWidth }) => {
         const newHeight = `calc(${$height} + ${$padding} * 2 + ${$borderWidth} * 2)`;
