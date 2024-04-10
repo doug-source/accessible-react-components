196e7ce0d01f1bb503b12dea12724fcb176118c9
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-09 21:44:33 -0300

    :recycle: refactor: Tabs test

--- src/components/molecules/Tabs/on.test.tsx
+++ src/components/molecules/Tabs/on.test.tsx
@@ -2,31 +2,40 @@ import '@testing-library/jest-dom';
 import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import 'jest-styled-components';
+import { ComponentPropsWithoutRef } from 'react';
 import { Tabs } from './index';
 
-function buildComponent() {
-    return render(
-        <Tabs title="main title" titles={['tab-1', 'tab-2']}>
-            <div>Text 1</div>
-            <div>Text 2</div>
-        </Tabs>
-    );
-}
+type ElementProps = ComponentPropsWithoutRef<typeof Tabs>;
+type keys = 'title' | 'titles' | 'children';
+type Props = Omit<ElementProps, keys> & Partial<Pick<ElementProps, keys>>;
+
+const buildComponent = ({
+    title = 'main title',
+    titles = ['tab-1', 'tab-2'],
+    ...remain
+}: Props = {}) => (
+    <Tabs title={title} titles={titles} {...remain}>
+        <div>Text 1</div>
+        <div>Text 2</div>
+    </Tabs>
+);
 
 describe('<Tabs /> component', () => {
     test('renders correctly', () => {
-        buildComponent();
+        render(buildComponent());
         const $el = screen.getByTitle('tabs');
         expect($el).toBeVisible();
     });
     test('renders each internal component correctly', () => {
-        buildComponent();
+        render(buildComponent());
         const $el = screen.getByTitle('tabs');
         expect($el).toBeVisible();
-        (($heading: HTMLElement) => {
-            expect($heading).toBeInTheDocument();
-            expect($heading).not.toBeVisible();
-        })(within($el).getByRole('heading', { level: 3, hidden: true }));
+        const $heading = within($el).getByRole('heading', {
+            level: 3,
+            hidden: true,
+        });
+        expect($heading).toBeInTheDocument();
+        expect($heading).not.toBeVisible();
         expect(within($el).getByRole('tablist')).toBeVisible();
         const panels = within($el).getAllByRole('tabpanel');
         panels.forEach(($panel, i) => {
@@ -35,23 +44,14 @@ describe('<Tabs /> component', () => {
         });
     });
     test('renders when orientation property changes correctly', () => {
-        const { rerender } = buildComponent();
+        const { rerender } = render(buildComponent());
         const $el = screen.getByTitle('tabs');
         expect($el).toHaveStyleRule('flex-direction', 'column');
-        rerender(
-            <Tabs
-                title="main title"
-                titles={['tab-1', 'tab-2']}
-                orientation="vertical"
-            >
-                <div>Text 1</div>
-                <div>Text 2</div>
-            </Tabs>
-        );
+        rerender(buildComponent({ orientation: 'vertical' }));
         expect($el).toHaveStyleRule('flex-direction', 'row');
     });
     test('changes the active tab after user click', async () => {
-        buildComponent();
+        render(buildComponent());
         const tabs = screen.getAllByRole('tab');
         expect(tabs[0]).toHaveAttribute('aria-selected', 'true');
         const user = userEvent.setup();
