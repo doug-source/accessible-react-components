c689d28dabea4d0fb65133080003066f69889443
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-12 14:11:52 -0300

    :white_check_mark: test: Improving keydown handler's test

--- src/components/molecules/SwitchBasic/lib/on-index.test.ts
+++ src/components/molecules/SwitchBasic/lib/on-index.test.ts
@@ -1,9 +1,40 @@
+import { KeyboardEvent } from 'react';
 import { makeKeydownHandler } from '.';
+
+const makeKeydownEventParam = (key: string) => {
+    return {
+        key,
+        preventDefault: jest.fn(),
+    } as unknown as KeyboardEvent<HTMLDivElement>;
+};
+
+const expectCalls = (
+    handler: ReturnType<typeof makeKeydownHandler>,
+    action: () => void,
+    key: string,
+    counter: number,
+    preventDefault = false
+) => {
+    const event = makeKeydownEventParam(key);
+    handler(event);
+    expect(action).toHaveBeenCalledTimes(counter);
+    if (preventDefault) {
+        expect(event.preventDefault).toHaveBeenCalled();
+    } else {
+        expect(event.preventDefault).not.toHaveBeenCalled();
+    }
+};
 
 describe('makeKeydownHandler funtion', () => {
     test('returns the keydown event handler function correctly', () => {
+        const handler = makeKeydownHandler(() => {});
+        expect(typeof handler).toBe('function');
+    });
+    test('triggers the action callback correctly', () => {
         const action = jest.fn();
         const handler = makeKeydownHandler(action);
-        expect(typeof handler).toBe('function');
+        expectCalls(handler, action, ' ', 1, true);
+        expectCalls(handler, action, 'Enter', 2, true);
+        expectCalls(handler, action, 'ArrowRight', 2);
     });
 });
