cdae5d0746c2fb39e0ab6019f765253ab71a333f
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-14 11:06:23 -0300

    :books: docs: React components' docs

--- src/components/atoms/DialogBtn/index.tsx
+++ src/components/atoms/DialogBtn/index.tsx
@@ -3,6 +3,12 @@ import { Button_ } from './style';
 
 type DialogBtnProps = ComponentPropsWithoutRef<'button'>;
 
+/**
+ * The HtmlButtonElement usually used into AlertDialog
+ *
+ * @param props The component's properties
+ * @returns The react component
+ */
 export const DialogBtn = (props: DialogBtnProps) => (
     <Button_ {...props} type="button" />
 );
--- src/components/molecules/AlertDialogActions/index.tsx
+++ src/components/molecules/AlertDialogActions/index.tsx
@@ -5,6 +5,12 @@ type AlertDialogActionsProps = ComponentPropsWithoutRef<
     typeof AlertDialogActions_
 >;
 
+/**
+ * Used as AlertDialog's children
+ *
+ * @param children The wrapper usually used to wrap dialog's buttons
+ * @returns The HTMLDivElement wrapper
+ */
 export const AlertDialogActions = ({
     children,
     ...remain
