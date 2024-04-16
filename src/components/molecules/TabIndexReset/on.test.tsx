c08fde87ebd25f70febf857cde2c9e8e39a2ba7d
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-16 14:03:02 -0300

    :wastebasket: remove: Unused import

--- src/components/molecules/TabIndexReset/on.test.tsx
+++ src/components/molecules/TabIndexReset/on.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import 'jest-styled-components';
 import { TabIndexReset } from './index';
 
 describe('<TabIndexReset /> component', () => {
