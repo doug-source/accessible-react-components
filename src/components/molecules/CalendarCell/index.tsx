85b9c0e730c25ac18da9d3958aad16aa79f4759a
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-25 14:19:06 -0300

    :sparkles: feat: ref passing into CalendarCell

--- src/components/molecules/CalendarCell/index.tsx
+++ src/components/molecules/CalendarCell/index.tsx
@@ -1,23 +1,24 @@
 import classNames from 'classnames';
-import React, { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, createElement, forwardRef } from 'react';
 import styles from './CalendarCell.module.scss';
 
-type CalendarCellProps<T extends 'th' | 'td'> = ComponentPropsWithoutRef<T> & {
-    type: T;
+type CalendarCellProps = ComponentPropsWithoutRef<'th' | 'td'> & {
+    type: 'th' | 'td';
 };
 
-export const CalendarCell = <T extends 'th' | 'td'>({
-    type,
-    className,
-    children,
-    ...remain
-}: CalendarCellProps<T>) => {
-    return React.createElement(
-        type,
-        {
-            ...remain,
-            className: classNames(className, styles.calendarCell),
-        },
-        children
-    );
-};
+export const CalendarCell = forwardRef<HTMLTableCellElement, CalendarCellProps>(
+    function CalendarCellInner(
+        { type, className, children, ...remain }: CalendarCellProps,
+        ref
+    ) {
+        return createElement(
+            type,
+            {
+                ...remain,
+                className: classNames(className, styles.calendarCell),
+                ref,
+            },
+            children
+        );
+    }
+);
