1dc6488da1175cf11104860e9a72b554c15b5121
Douglas Leandro <douglas.gtads@gmail.com>
2024-04-25 15:11:16 -0300

    :sparkles: feat: ref passing into BtnDefault

--- src/components/atoms/BtnDefault/index.tsx
+++ src/components/atoms/BtnDefault/index.tsx
@@ -1,20 +1,19 @@
 import classNames from 'classnames';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import styles from './BtnDefault.module.scss';
 
 type BtnDefaultProps = ComponentPropsWithoutRef<'button'>;
 
-export const BtnDefault = ({
-    className,
-    children,
-    ...remain
-}: BtnDefaultProps) => {
-    return (
-        <button
-            {...remain}
-            className={classNames(className, styles.btnDefault)}
-        >
-            {children}
-        </button>
-    );
-};
+export const BtnDefault = forwardRef<HTMLButtonElement, BtnDefaultProps>(
+    function BtnDefaultInner({ className, children, ...remain }, ref) {
+        return (
+            <button
+                {...remain}
+                ref={ref}
+                className={classNames(className, styles.btnDefault)}
+            >
+                {children}
+            </button>
+        );
+    }
+);
