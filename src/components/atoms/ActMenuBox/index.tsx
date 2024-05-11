0b7dcb01c393da2fa60a4dd67f87d37a1613ff3e
Douglas Leandro <douglas.gtads@gmail.com>
2024-05-11 15:27:51 -0300

    :sparkles: feat: applying forwardRef into ActMenuBox

--- src/components/atoms/ActMenuBox/index.tsx
+++ src/components/atoms/ActMenuBox/index.tsx
@@ -1,26 +1,29 @@
 import classNames from 'classnames';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import styles from './ActMenuBox.module.scss';
 
 type ActMenuBoxProps = ComponentPropsWithoutRef<'ul'> & {
     expanded: boolean;
 };
 
-export const ActMenuBox = ({
-    className,
-    expanded,
-    children,
-    ...remain
-}: ActMenuBoxProps) => (
-    <ul
-        {...remain}
-        role="menu"
-        className={classNames(
-            className,
-            styles.actMenuBox,
-            expanded ? styles.show : styles.hide
-        )}
-    >
-        {children}
-    </ul>
+export const ActMenuBox = forwardRef<HTMLUListElement, ActMenuBoxProps>(
+    function ActMenuBoxInner(
+        { className, expanded, children, ...remain }: ActMenuBoxProps,
+        ref
+    ) {
+        return (
+            <ul
+                {...remain}
+                ref={ref}
+                role="menu"
+                className={classNames(
+                    className,
+                    styles.actMenuBox,
+                    expanded ? styles.show : styles.hide
+                )}
+            >
+                {children}
+            </ul>
+        );
+    }
 );
