19f0453c0719a6d35ce1b39800b96b3f01e7e18e
Douglas Leandro <douglas.gtads@gmail.com>
2024-05-22 18:52:24 -0300

    :white_check_mark: test: adjusting ActMenuItem to MenuItem

--- src/components/atoms/MenuItem/on.test.tsx
+++ src/components/atoms/MenuItem/on.test.tsx
@@ -1,37 +1,34 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { ComponentPropsWithoutRef } from 'react';
 import styles from './MenuItem.module.scss';
-import { ActMenuItem } from './index';
-
-type ElementProps = ComponentPropsWithoutRef<typeof ActMenuItem>;
-type keys = 'listRefFn';
-type Props = Omit<ElementProps, keys> & Partial<Pick<ElementProps, keys>>;
-
-const buildComponent = ({
-    listRefFn = () => {},
-    children = 'list item content',
-    ...remain
-}: Props = {}) => (
-    <ul>
-        <ActMenuItem {...remain} listRefFn={listRefFn}>
-            {children}
-        </ActMenuItem>
-    </ul>
-);
+import { MenuItem } from './index';
 
 describe('<MenuItem /> component', () => {
     test('renders correctly', () => {
-        render(buildComponent());
+        render(
+            <ul>
+                <MenuItem listRefFn={() => {}}>list item content</MenuItem>
+            </ul>
+        );
         const $el = screen.getByText('list item content');
         expect($el).toBeInTheDocument();
         expect($el).toHaveClass(styles.menuItem);
     });
     test('renders with and no identifier correctly', () => {
-        const { rerender } = render(buildComponent());
+        const { rerender } = render(
+            <ul>
+                <MenuItem listRefFn={() => {}}>list item content</MenuItem>
+            </ul>
+        );
         const $el = screen.getByText('list item content');
         expect($el).not.toHaveAttribute('id');
-        rerender(buildComponent({ identified: true }));
+        rerender(
+            <ul>
+                <MenuItem identified listRefFn={() => {}}>
+                    list item content
+                </MenuItem>
+            </ul>
+        );
         expect($el).toHaveAttribute('id');
     });
 });
